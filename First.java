df7087faee7773be02280f461d8feea1d5634f09
w2yuri <namatro3@gmail.com>
2024-06-05 23:44:45 +0900

    複数行と1行のコメントアウト確認

--- First.java
+++ First.java
@@ -1,5 +1,8 @@
+/* 文字列を表示するプログラム
+2024.06.05 */ 
+
 class First {
   public static void main(String[] args) {
-    System.out.println("Javaを楽しもう！");
+    System.out.println("Javaを楽しもう！");   //  画面表示
   }
 }
