ef8937a1eb8ceae038c4ac586c8d7cdec45087e9
w2yuri <namatro3@gmail.com>
2024-06-06 14:24:16 +0900

    [追加〜完成]演習２(2-5)

---
+++ Show.java
@@ -0,0 +1,18 @@
+class Show {
+    public static void main (String[] args) {
+    //   変数の宣言と値の代入
+    // データ型　　変数名　　＝　　値　　；(「初期化」をしている)
+     int num = 10;
+     double pi = 3.14;
+     char blood = 'A';
+     boolean fun = true;
+     String msg = "確認";
+
+    //   値の表示
+    System. out.println(num);
+    System. out.println(pi);
+    System. out.println(blood);
+    System. out.println(fun);
+    System. out.println(msg);
+    }
+}
