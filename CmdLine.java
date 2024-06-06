dde4755c08930a1bd882da67f5f66e4e844456a2
w2yuri <namatro3@gmail.com>
2024-06-06 14:02:23 +0900

    [追加〜完成]コマンドラインファイル(2-4)

---
+++ CmdLine.java
@@ -0,0 +1,8 @@
+class CmdLine {
+    public static void main(String[] args) {
+       System. out.println("args[0]:" + args[0]);
+       System. out.println("args[1]:" + args[1]);
+       System. out.println("args.length : " + args. length);
+    //    args. length = 何個のデータがコマンドラインから入力されたか取得して画面に表示できる
+    }
+}
