106771b44df24b36f21dcd972e7ca6857cbabb82
w2yuri <namatro3@gmail.com>
2024-06-07 21:40:39 +0900

    [追加〜完成]else-if文(2-8)

---
+++ Branch2.java
@@ -0,0 +1,22 @@
+class Branch2 {
+    public static void main(String[] args) {
+        // コマンドラインに入力された価格をint型に変換する
+        int price = Integer.parseInt(args[0]);
+        double rate = 0.10;   //消費税率１０％()
+        // int型の変数を2つ指定(discount(値引額)amount)
+        int discount, amount;
+
+        if (price >= 5000 ){  //割引額の設定
+            discount = 500;   //true=5000円以上で500円の値引き
+        }   else if( price >= 3000 ) {
+            discount = 300;   //true=3000円以上で300円の値引き
+        }   else  {
+            discount = 0;     //その他(2999円以下)割引なし
+        }
+
+        // 税込金額を計算した結果が少数なので、int型にキャストする
+        amount = (int)((price - discount) * (1 + rate));
+        System. out.println("割引金額" + discount + "円");
+        System. out.println("税込金額" + amount + "円");
+    }
+}
