f13a6a73d1993578cb859d4512f690276dbe9bed
w2yuri <namatro3@gmail.com>
2024-06-07 22:12:50 +0900

    [追加〜完成]while文(2-９)

---
+++ Loop1.java
@@ -0,0 +1,12 @@
+class Loop1 {
+    public static void main(String[] args) {
+        // ①最初の作業：処理の回数を数えるための変数を0で初期化する
+        int i = 0;
+        while( i < 3 ) {
+            // ②繰り返し処理をする
+            System. out.println("i = " + i);
+            // ③その変数をカウントアップ
+            i++;
+        }
+    }
+}
