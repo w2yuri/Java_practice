f908fc5ee75eadb5e76c657eb2d3773da9943584
w2yuri <namatro3@gmail.com>
2024-06-07 23:13:14 +0900

    [追加〜完成]演習4(2-10)

---
+++ LoopMessage.java
@@ -0,0 +1,10 @@
+class LoopMessage {
+    public static void main(String[] args) {
+        //メッセージを10回表示
+        int i = 1;
+        while(i <= 10) {
+           System.out.println(i + "回目：猫ふわふわ！");
+           i++;
+        }
+    }
+}
