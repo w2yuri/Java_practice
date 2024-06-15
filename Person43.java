3bcf162c7d0baac2320f8bce44aec8535994f1c8
w2yuri <namatro3@gmail.com>
2024-06-15 23:12:38 +0900

    [完成]thisとsuper(4-3)_2

--- Person43.java
+++ Person43.java
@@ -2,6 +2,7 @@ public class Person43 {
     private String name;
 
     public Person43(String name){
+        //thisは二行目のnameを指している
         this.name = name;
     }
     public void display(){
--- Student43.java
+++ Student43.java
@@ -1,13 +1,17 @@
 public class Student43 extends Person43{
     private int stuNo;
 
+    //コンストラクタ①名前だけ引数として受け取るコンストラクタ
     public Student43(String name){
+        //this~＝引数の呼び出し(=Student43を指す)
         this(name, 999);
     }
+    //コンストラクタ②名前と学籍番号を引数として受け取るコンストラクタ
     public Student43(String name, int stuNo){
         super(name);
         this.stuNo = stuNo;
     }
+    //スーパークラスのdisplayをオーバーライドしている
     public void display(){
         super.display();
         System.out.println("学籍番号:" + stuNo);
