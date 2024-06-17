c37d117cc8895eebf679f57e8fc606136c986797
w2yuri <namatro3@gmail.com>
2024-06-17 19:34:19 +0900

    [※エラー修正：要]抽象クラスとポリモーフィズム(4-7)

---
+++ Club.java
@@ -0,0 +1,11 @@
+public abstract class Club {
+    private String name;  //部活名
+    
+    public Club(String name){
+        this.name = name;
+    }
+    public void display(){
+        System.out.println("部活動: " + name);
+    }
+    public abstract void practice();
+}
---
+++ StuSample47.java
@@ -0,0 +1,14 @@
+public class StuSample47 {
+    public static void main(String[] args){
+        TandF taf = new TandF("陸上競技部");
+        Football fb = new Football("サッカー部");
+
+        Student47 stu1 = new Student47("菅原", taf);
+        stu1.display();
+        stu1.practice();
+
+        Student47 stu2 = new Student47("桜井", fb);
+        stu2.display();
+        stu2.practice();
+    }
+}
